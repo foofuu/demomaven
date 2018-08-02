7b4ec35b8e6d1fe70fa3d1092a3fe359bd472102
周隽夫 <zhoujunfuv5@163.com>
2018-08-02 20:17:10 +0800

    demo codes

--- src/main/java/com/fufu/algorithm/sort/InsertSort.java
+++ src/main/java/com/fufu/algorithm/sort/InsertSort.java
@@ -6,5 +6,15 @@ package com.fufu.algorithm.sort;
 public class InsertSort {
     public static void sort(int[] a) {
         int length = a.length;
+        int temp;
+        for (int i = 1; i < a.length; i++) {
+            temp = a[i];
+            int j = i - 1;
+            while (j>=0&&a[j] > temp) {
+                a[j+1] = a[j];
+                j--;
+            }
+            a[j+1] = temp;
+        }
     }
 }
--- src/main/java/com/fufu/algorithm/sort/Main.java
+++ src/main/java/com/fufu/algorithm/sort/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         int[] a = new int[]{3,1,2,5,7,6,8,4};
         System.out.println(Arrays.toString(a));
-        BubbleSort.sort(a);
+        InsertSort.sort(a);
         System.out.println(Arrays.toString(a));
     }
 }
