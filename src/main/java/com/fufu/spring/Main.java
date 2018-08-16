bddbd1af3c22c89e3c8347fef0256d3564391a11
周隽夫 <zhoujunfuv5@163.com>
2018-08-16 19:23:52 +0800

    demo codes

---
+++ src/main/java/com/fufu/spring/Main.java
@@ -0,0 +1,22 @@
+package com.fufu.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Created by zhoujunfu on 2018/8/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("现在开始初始化容器");
+        ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("spring.xml");
+        System.out.println("容器初始化成功");
+
+        Person person = (Person) applicationContext.getBean("person");
+        System.out.println(person);
+
+        System.out.println("现在开始关闭容器！");
+        applicationContext.registerShutdownHook();
+    }
+}
